20017f8cdd4bd61149482f9145c816055610d969
Peter Singh <28299972+aFuzzyBear@users.noreply.github.com>
2024-02-26 14:01:46 +0000

    refactored santizeURL function

--- src/utils/getVersion.ts
+++ src/utils/getVersion.ts
@@ -10,14 +10,28 @@ declare global {
 const launchOpts = process?.env.CHROMIUM_FLAGS
   ? { args: process?.env.CHROMIUM_FLAGS.split(/\s+/) }
   : {};
-
+  
+  function sanitizeURL(inputURL) {
+    try {
+      let url = new URL(inputURL);
+      return url.toString(); // URL is valid and normalized
+    } catch (error) {
+      // Input is not a valid URL; attempt to prepend "http://" or "https://"
+      try {
+        let url = new URL(`https://${inputURL}`);
+        return url.toString(); // Assume HTTPS if the scheme was missing
+      } catch (error) {
+        throw new Error('Invalid URL provided.');
+      }
+    }
+  }
 let browser
 export async function checkForJQuery(url) {
-  const sanitizeURL = !url.match(/http|https|www/g) ? new URL(`https://www.${url}`.toLowerCase()).toString() : new URL(url.toLowerCase()).toString()
+  const cleanURL = sanitizeURL(url)
   browser = await puppeteer.launch(launchOpts);
   const page = await browser.newPage();
   try {
-    await page.goto(sanitizeURL, {
+    await page.goto(cleanURL, {
       timeout: 60000,
       waitUntil: "domcontentloaded"
     });
